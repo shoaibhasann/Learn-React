cba87030f4ef88f3a4bb8a94fec5985a32ebcc85
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-29 01:18:09 +0530

    Fix bugs in word count logic

--- Learn-React/src/components/Form.jsx
+++ Learn-React/src/components/Form.jsx
@@ -16,26 +16,31 @@ function Form() {
     setText("");
   };
 
-const handleCopy = () => {
-  // Check if the inputRef.current is available (not null)
-  if (inputRef.current) {
-    // Access the value of the input element using inputRef.current.value
-    const textToCopy = inputRef.current.value;
+  const handleCopy = () => {
+    // Check if the inputRef.current is available (not null)
+    if (inputRef.current) {
+      // Access the value of the input element using inputRef.current.value
+      const textToCopy = inputRef.current.value;
 
-    // Use the Clipboard API to write the text to the clipboard
-    navigator.clipboard
-      .writeText(textToCopy)
-      .then(() => {
-        // Text successfully copied to the clipboard
-        console.log("Text copied to clipboard:", textToCopy);
-      })
-      .catch((error) => {
-        // Failed to copy text to the clipboard
-        console.error("Failed to copy text:", error);
-      });
-  }
-};
+      // Use the Clipboard API to write the text to the clipboard
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          // Text successfully copied to the clipboard
+          console.log("Text copied to clipboard:", textToCopy);
+        })
+        .catch((error) => {
+          // Failed to copy text to the clipboard
+          console.error("Failed to copy text:", error);
+        });
+    }
+  };
 
+  // Calculate the word count by splitting the text by spaces
+  const wordCount = text.trim().split(/\s+/).length;
+
+  // Calculate the character count (including spaces)
+  const characterCount = text.length;
 
   return (
     <div className="mx-auto">
@@ -49,7 +54,7 @@ const handleCopy = () => {
       <button onClick={handleClick}>To Uppercase</button>
       <h2>Your text here</h2>
       <p>
-        {text.split(" ").length - 1} words {text.length} characters
+        {wordCount} words {characterCount} characters
       </p>
       <h3>Preview</h3>
       <p>{text}</p>
